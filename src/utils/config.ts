b26c9620963e3ee15198937d52ac1ae46fb9b789
Amit Shah <cingularity.net@gmail.com>
2022-01-01 20:54:38 +0100

    WIP

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,3 @@
 export const AUTH_TOKEN = "token";
 export const GRAPHQL_URL =
-  process.env.GRAPHQL_URL || "http://localhost:8080/graphql";
+  process.env.REACT_APP_GRAPHQL_URL || "http://localhost:8080/graphql";
