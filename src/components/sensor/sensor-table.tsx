69706b7dd0fb327e18e75a1e8b2d211f3cc36d43
nikky jaiswal <jaisnikky1@gmail.com>
2022-01-05 00:03:16 +0100

    Working on the table

--- src/components/sensor/sensor-table.tsx
+++ src/components/sensor/sensor-table.tsx
@@ -1,9 +1,10 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
-// import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { useQuery } from "@apollo/client";
 import { GET_SENSORS } from "../../operations/queries/sensors";
 import Loading from "../utilities/Loading";
+import { Box, TextField } from "@mui/material";
 
 interface Props {}
 
@@ -47,22 +48,48 @@ const columns: GridColDef[] = [
       params.row.spectrum.map((item: any) => item.name).join(", "),
     width: 180,
   },
+  { field: "optical_area_x" },
+  { field: "optical_area_y" },
+  { field: "exact_optical_area_diagonal" },
+  { field: "next_optical_class" },
+  { field: "aspect_ratio" },
+  { field: "housing_x" },
+  { field: "housing_y" },
+  { field: "optical_center_x" },
+  { field: "optical_center_y" },
+  { field: "center_shift_x" },
+  { field: "center_shift_y" },
+  { field: "housing_glass" },
+  { field: "glass_lid_thickness" },
+  { field: "focal_plane_from_bottom" },
+  { field: "glass_index" },
+  { field: "pixel_lens_cra" },
+  { field: "alternative_designation" },
+  { field: "other_info" },
 ];
 
 const SensorTable: FC<Props> = () => {
-  // const { t } = useTranslation();
+  const [search, setSearch] = useState("");
+  const { t } = useTranslation();
   const { data, loading, error } = useQuery(GET_SENSORS);
 
   if (loading) return <Loading />;
   if (error) return <>error occured</>;
-  console.log("data", data.sensors);
 
   return (
-    <div style={{ height: "90vh", width: "100%" }}>
+    <div style={{ height: "80vh", width: "100%" }}>
+      <Box sx={{ mt: 1, p: 1 }}>
+        <TextField
+          label={t("_search_text")}
+          variant="outlined"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </Box>
       <DataGrid
         rows={data.sensors}
         columns={columns}
-        pageSize={5}
+        pageSize={9}
         rowsPerPageOptions={[5]}
         // checkboxSelection
         disableSelectionOnClick
