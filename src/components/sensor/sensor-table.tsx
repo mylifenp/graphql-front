7c68583e5dc917018f60c342c21d5b92cf3243e7
nikky jaiswal <jaisnikky1@gmail.com>
2022-01-05 00:39:51 +0100

    WIP

--- src/components/sensor/sensor-table.tsx
+++ src/components/sensor/sensor-table.tsx
@@ -1,12 +1,27 @@
-import { FC, useState } from "react";
+import { FC, useState, useEffect } from "react";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import { useTranslation } from "react-i18next";
 import { useQuery } from "@apollo/client";
 import { GET_SENSORS } from "../../operations/queries/sensors";
 import Loading from "../utilities/Loading";
 import { Box, TextField } from "@mui/material";
+import { Sensor } from "../../interfaces/sensor";
+import Fuse from "fuse.js";
 
 interface Props {}
+
+const SensorFilter = (searchText: string, data: Sensor[]) => {
+  const options = {
+    keys: ["sensor_model"],
+    threshold: 0.0,
+    minMatchCharLength: searchText.length,
+    ignoreLocation: true,
+  };
+
+  const fuse = new Fuse(data, options);
+  let result = fuse.search(searchText);
+  return result.map((res) => res.item);
+};
 
 const columns: GridColDef[] = [
   {
@@ -73,6 +88,13 @@ const SensorTable: FC<Props> = () => {
   const { t } = useTranslation();
   const { data, loading, error } = useQuery(GET_SENSORS);
 
+  // data({
+  //   variables: {},
+  //   onCompleted: ({ sensors }: any) => {
+  //     console.log("sensors", sensors);
+  //   },
+  // });
+
   if (loading) return <Loading />;
   if (error) return <>error occured</>;
 
@@ -83,14 +105,16 @@ const SensorTable: FC<Props> = () => {
           label={t("_search_text")}
           variant="outlined"
           value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
         />
       </Box>
       <DataGrid
         rows={data.sensors}
         columns={columns}
         pageSize={9}
-        rowsPerPageOptions={[5]}
+        rowsPerPageOptions={[9]}
         // checkboxSelection
         disableSelectionOnClick
       />
@@ -99,75 +123,3 @@ const SensorTable: FC<Props> = () => {
 };
 
 export default SensorTable;
-
-// import { useQuery } from "@apollo/client";
-// import { Box, Card } from "@mui/material";
-// import { FC } from "react";
-// // import { useTranslation } from "react-i18next";
-// import { Sensor } from "../../interfaces/sensor";
-// import { GET_SENSORS } from "../../operations/queries/sensors";
-// import DataTable from "../table";
-
-// interface Props {}
-
-// const columns = [
-//   {
-//     name: "Supplier",
-//     sortable: true,
-//     selector: (row: Sensor) => {
-//       const { supplier } = row;
-//       if (!supplier) return "";
-//       return supplier.name;
-//     },
-//   },
-//   {
-//     name: "Sensor Model",
-//     selector: (row: Sensor) => row.sensor_model || "",
-//     sortable: true,
-//   },
-//   {
-//     name: "Pixel Size",
-//     selector: (row: Sensor) => row.pixel_size || "",
-//     sortable: true,
-//   },
-//   {
-//     name: "End Of Life",
-//     selector: (row: Sensor) => row.end_of_life || "",
-//     sortable: true,
-//   },
-//   {
-//     name: "Aspect Ratio",
-//     selector: (row: Sensor) => row.aspect_ratio || "",
-//   },
-// ];
-
-// // const data: Data[] = [
-// //   {
-// //     id: 1,
-// //     title: "Beetlejuice",
-// //     year: "1988",
-// //   },
-// //   {
-// //     id: 2,
-// //     title: "Ghostbusters",
-// //     year: "1984",
-// //   },
-// // ];
-
-// const SensorTable: FC<Props> = () => {
-//   // const { t } = useTranslation();
-//   const { data, loading, error } = useQuery(GET_SENSORS);
-
-//   if (loading) return <p>"Submitting..."</p>;
-//   if (error) return <p>{`Submission error! ${error.message}`}</p>;
-
-//   return (
-//     <Card sx={{ m: 1 }}>
-//       <Box sx={{ m: 1, p: 1 }}>
-//         <DataTable columns={columns} data={data?.sensors} />
-//       </Box>
-//     </Card>
-//   );
-// };
-
-// export default SensorTable;
